29ada015288f2254d9883d3e0a72af367723d7cd
Lucas-Godoy <Pedrolucf@gmail.com>
2024-02-02 15:54:11 -0300

    adding tempo and fps

--- pong.asm
+++ pong.asm
@@ -4,6 +4,8 @@ STACK ENDS
 
 DATA SEGMENT PARA 'DATA'
 
+    TIME_AUX  DB 0      ;variable used when checking if time has changed
+
     BALL_X    DW 0Ah    ;x position (column) of the bal
     BALL_Y    DW 0Ah    ;y position (line) of the ball
     BALL_SIZE DW 04h    ;size of the ball (how many pixels does the ball have in with and heigth)
@@ -30,8 +32,19 @@ MAIN PROC FAR
                          MOV    BH,00h                      ; to the background color
                          MOV    BL,00h                      ;chosing black as background color
                          INT    10h                         ; execute the configuration
+    
+    CHECK_TIME:          
+                         MOV    AH,2Ch                      ;get the system time
+                         INT    21h                         ;CH = hour CL = minute DH = second DL =1/100 seconds
 
+                         CMP    DL,TIME_AUX                 ;is the current time equal top the previous one(TIME_AUX)?
+                         JE     CHECK_TIME                  ;if it is the smae, check again
+    ;if isn't, then drawn, move,etc.
+
+                         MOV    TIME_AUX,DL                 ;update the time
                          CALL   DRAW_BALL                   ;calls the process of drawing the
+
+                         JMP    CHECK_TIME                  ;after everthing checks time again
 
                          RET
 
